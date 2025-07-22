17594598a42dd3d12ea7bc19e82aa2fa659b2603
Yashigami <kuzmin_var@bk.ru>
2025-07-22 15:04:58 +0300

    feat: создана базовая структура HTTP-сервера

---
+++ cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	todo "todo-app"
+	"todo-app/pkg/handler"
+)
+
+func main() {
+	handlers := new(handler.Handler)
+	srv := new(todo.Server)
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while http server: %s", err.Error())
+	}
+}
