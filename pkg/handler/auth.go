afa87b4ca0440d73c1c8a981947720ffe6ed0c19
Yashigami <kuzmin_var@bk.ru>
2025-07-28 20:45:30 +0300

    feat: Реализованна аутентификация и получение токена.

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -2,13 +2,16 @@ package handler
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
 	todo "todo-app"
 )
 
 func (h *Handler) signUp(c *gin.Context) {
+	log.Println("signUp endpoint hit")
+
 	var input todo.User
- 
+
 	if err := c.BindJSON(&input); err != nil {
 		newErrorResponse(c, http.StatusBadRequest, err.Error())
 		return
@@ -24,6 +27,28 @@ func (h *Handler) signUp(c *gin.Context) {
 	})
 }
 
+type signInInput struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
 func (h *Handler) signIn(c *gin.Context) {
+	log.Println("signIn endpoint hit")
+
+	var input signInInput
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	token, err := h.services.Authorization.GenerateToken(input.Username, input.Password)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"token": token,
+	})
 
 }
